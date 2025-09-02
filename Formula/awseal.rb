3009f8e722351d0f17ee05cc40f369a98e8e68bf
Andy Caine <andy@hyperscale.consulting>
2025-09-02 21:49:13 +0100

    initial commit

---
+++ Formula/awseal.rb
@@ -0,0 +1,15 @@
+class Awseal < Formula
+  desc "A secure AWS credential manager that protects your AWS SSO credentials using Apple's Secure Enclave"
+  homepage "https://github.com/hyperscal-consulting/awseal"
+  url "https://github.com/hyperscal-consulting/awseal/releases/download/v0.1.1/awseal-0.1.1.tar.gz"
+  sha256 "ad4e50e46ae6dc464a65a5cf02ceb1166b3a9dab904f69aeffb222ca2cd949d3"
+  version "0.1.1"
+
+  def install
+    bin.install "awseal"
+  end
+
+  test do
+    system "#{bin}/awseal", "--version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+# Hyperscale Homebrew Tap
+
+## How do I install these formulae?
+
+`brew install hyperscale-consulting/hyperscale/<formula>`
+
+Or `brew tap hyperscale-consulting/hyperscale` and then `brew install <formula>`.
+
+Or, in a `brew bundle` `Brewfile`:
+
+```ruby
+tap "hyperscale-consulting/hyperscale"
+brew "<formula>"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
