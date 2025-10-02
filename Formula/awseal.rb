d0e0a5b26dbbe7fed46e946646d8cf8a826efd09
Andy Caine <andy@hyperscale.consulting>
2025-10-02 20:01:11 +0100

    corrected url

--- Formula/awseal.rb
+++ Formula/awseal.rb
@@ -1,7 +1,7 @@
 class Awseal < Formula
   desc "A secure AWS credential manager that protects your AWS credentials using Apple's Secure Enclave"
   homepage "https://github.com/hyperscal-consulting/awseal"
-  url "https://github.com/hyperscale-consulting/awseal/releases/download/v0.3.0/awseal-v0.3.0.tar.gz"
+  url "https://github.com/hyperscale-consulting/awseal/releases/download/v0.3.1/awseal-v0.3.1.tar.gz"
   sha256 "96e4b849c51e76efad9e67cf1bc62a83c1f87d99647497fd4012fdf912a82c66"
   version "0.3.1"
 
